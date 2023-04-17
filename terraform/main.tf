75a16b758a572cd2526ea81d62b7b1219a1f4d22
Pauline Maudet <pauline.maudet@globalis-ms.com>
2023-04-17 11:50:04 +0200

    Fix workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,16 +60,50 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
+  name                = "${var.prefix}-nic2"
   resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                      = "${var.prefix}-nic2"
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
   }
+}
+
+resource "azurerm_network_security_group" "webserver" {
+  name                = "tls_webserver"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "tls"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "8000"
+    destination_address_prefix = azurerm_network_interface.main.private_ip_address
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.internal.id
+  network_security_group_id = azurerm_network_security_group.webserver.id
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
@@ -80,6 +114,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.main.id,
+    azurerm_network_interface.internal.id
   ]
 
   admin_ssh_key {
@@ -108,76 +143,3 @@ output "vm_public_ip_address" {
 output "vm_username" {
   value = azurerm_linux_virtual_machine.main.admin_username
 }
-
-#resource "azurerm_network_interface" "main" {
-#  name                = "${var.prefix}-nic1"
-#  resource_group_name = azurerm_resource_group.main.name
-#  location            = azurerm_resource_group.main.location
-#
-#  ip_configuration {
-#    name                          = "primary"
-#    subnet_id                     = azurerm_subnet.internal.id
-#    private_ip_address_allocation = "Dynamic"
-#    public_ip_address_id          = azurerm_public_ip.pip.id
-#  }
-#}
-#
-#resource "azurerm_network_interface" "internal" {
-#  name                      = "${var.prefix}-nic2"
-#  resource_group_name       = azurerm_resource_group.main.name
-#  location                  = azurerm_resource_group.main.location
-#
-#  ip_configuration {
-#    name                          = "internal"
-#    subnet_id                     = azurerm_subnet.internal.id
-#    private_ip_address_allocation = "Dynamic"
-#  }
-#}
-#
-#resource "azurerm_network_security_group" "webserver" {
-#  name                = "tls_webserver"
-#  location            = azurerm_resource_group.main.location
-#  resource_group_name = azurerm_resource_group.main.name
-#  security_rule {
-#    access                     = "Allow"
-#    direction                  = "Inbound"
-#    name                       = "tls"
-#    priority                   = 100
-#    protocol                   = "Tcp"
-#    source_port_range          = "*"
-#    source_address_prefix      = "*"
-#    destination_port_range     = "443"
-#    destination_address_prefix = azurerm_network_interface.main.private_ip_address
-#  }
-#}
-#
-#resource "azurerm_network_interface_security_group_association" "main" {
-#  network_interface_id      = azurerm_network_interface.internal.id
-#  network_security_group_id = azurerm_network_security_group.webserver.id
-#}
-#
-#resource "azurerm_linux_virtual_machine" "main" {
-#  name                            = "${var.prefix}-vm"
-#  resource_group_name             = azurerm_resource_group.main.name
-#  location                        = azurerm_resource_group.main.location
-#  size                            = "Standard_F2"
-#  admin_username                  = "adminuser"
-#  admin_password                  = "P@ssw0rd1234!"
-#  disable_password_authentication = false
-#  network_interface_ids = [
-#    azurerm_network_interface.main.id,
-#    azurerm_network_interface.internal.id,
-#  ]
-#
-#  source_image_reference {
-#    publisher = "Canonical"
-#    offer     = "UbuntuServer"
-#    sku       = "16.04-LTS"
-#    version   = "latest"
-#  }
-#
-#  os_disk {
-#    storage_account_type = "Standard_LRS"
-#    caching              = "ReadWrite"
-#  }
-#}
